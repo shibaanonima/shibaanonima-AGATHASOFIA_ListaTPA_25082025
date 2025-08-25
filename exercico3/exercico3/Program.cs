4428110a26b6b255129149797598528b0936f031
Sofia <shibaanonima@gmail.com>
2025-08-25 12:39:54 -0300

    Add files via upload

---
+++ exercico3/exercico3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("digite um numero:");
+                a = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{a} x {i} = {a * i}");
+            }
+        
+        }
+    }
+}
