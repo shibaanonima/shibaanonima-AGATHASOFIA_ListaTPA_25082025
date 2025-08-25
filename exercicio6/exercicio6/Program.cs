1567021e56f94fe6589878f92218f34785a1aa43
Sofia <shibaanonima@gmail.com>
2025-08-25 12:41:24 -0300

    Add files via upload

---
+++ exercicio6/exercicio6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            b = 0;
+           
+            for (int i = 1; i <= 10; i++)
+            {
+
+
+                Console.Write($"digite sua {i}º nota:");
+                a = int.Parse(Console.ReadLine());
+
+                if (a >= 0 && a <= 10)
+                {
+                    b = a + b;
+                }
+
+                else
+                {
+                    Console.WriteLine("numero invalido");
+                }
+            }
+                  int c = b / 10;
+                 Console.WriteLine($"A media das suas notas é: {c}");
+             
+        }
+    }
+}
