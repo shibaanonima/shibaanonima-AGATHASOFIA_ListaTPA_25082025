5da60bc8690a91fb99460db4c7b4e0adb90579a6
Sofia <shibaanonima@gmail.com>
2025-08-25 18:49:20 -0300

    Add files via upload

---
+++ exercicio7/exercicio7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <=100; i++)
+            {
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine($"{i} É MÚLTIPLO DE 10");
+                }
+                else
+                    Console.WriteLine(i); 
+                }
+
+        }
+    }
+}
