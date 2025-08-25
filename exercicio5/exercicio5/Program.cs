e2e20aba34223fa254ab24aba76e0a08c41c003d
Sofia <shibaanonima@gmail.com>
2025-08-25 12:40:50 -0300

    Add files via upload

---
+++ exercicio5/exercicio5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero, maior;
+
+        Console.Write("Digite o 1º número: ");
+        numero = int.Parse(Console.ReadLine());
+
+        maior = numero;
+
+        for (int i = 2; i <= 15; i++)
+        {
+            Console.Write($"Digite o {i}º número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero > maior)
+            {
+                maior = numero;
+            }
+        }
+
+        Console.WriteLine($"\nO maior número digitado foi: {maior}");
+    }
+}
+
