7e86d88b1895b840c9158a3af72189d6fd01e7fc
Shiba <Shiba@SHIBA>
2025-08-25 12:33:23 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        for (int i = 11; i <= 250; i++)
+        {
+            
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
