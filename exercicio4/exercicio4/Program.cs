fa53eeecc17b8b97159a2f70d50e64dbd89f0ff6
Sofia <shibaanonima@gmail.com>
2025-08-25 12:40:19 -0300

    Add files via upload

---
+++ exercicio4/exercicio4/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c;
+            c = 0;
+            Console.Write("Digite um numero:");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Digite outro numero:");
+            b = int.Parse(Console.ReadLine());
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 2 != 0)
+
+                    c = c + 1;
+            }
+            Console.WriteLine($" existem {c} numeros impares entre {a} e {b}");
+            Console.ReadKey();
+        }
+    }
+}
