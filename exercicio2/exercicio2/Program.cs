d8051c4d7f36b2eecab00145f60abe9c401cf886
Sofia <shibaanonima@gmail.com>
2025-08-25 12:39:21 -0300

    Add files via upload

---
+++ exercicio2/exercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, b;
+        Console.Write("digite um numero menor:");
+        a = int.Parse(Console.ReadLine());
+        Console.Write("digite outro numero maior:");
+        b = int.Parse(Console.ReadLine());
+        if ( a > b)
+        {
+            Console.WriteLine("erro, você preencheu os campos de forma incorreta");
+        }
+        for (int i = b; i >= a; i--)
+        {
+            if (i % 2 == 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
