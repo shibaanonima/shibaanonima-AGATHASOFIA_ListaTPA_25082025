386c38571153a40d905dd281a8c1a5e92b26b943
Sofia <shibaanonima@gmail.com>
2025-08-25 18:50:18 -0300

    Add files via upload

---
+++ exercicio8/exercicio8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a; 
+            double b;
+            b = 1;
+            Console.Write("Digite um número:");
+            a = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= a; i++)
+            {
+                Console.WriteLine(b);
+                b = b * i;
+                
+
+         
+            }
+            Console.WriteLine(b);
+        }
+    }
+}
